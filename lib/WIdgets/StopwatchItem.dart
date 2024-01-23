46e53c4ecfcb932153c0253e51973fb9b8b559c0
fluttervieh <lorenz.richard@gmx.at>
2024-01-23 17:57:48 +0100

    added stopwatch labeling call

--- lib/WIdgets/StopwatchItem.dart
+++ lib/WIdgets/StopwatchItem.dart
@@ -1,11 +1,16 @@
 import "dart:async";
+import "package:ass/API/APIController.dart";
 import "package:ass/DTO/Device/DeviceDTO.dart";
+import "package:ass/DTO/SmartMeter/SmartMeterDataDTO.dart";
 import "package:ass/DTO/StopWatchHoldItem.dart";
+import "package:ass/HelperClasses/StatusCodes.dart";
+import "package:ass/Services/TokenCheckService.dart";
+import "package:ass/WIdgets/BottomToast.dart";
 import "package:flutter/material.dart";
 import "package:ass/Themes/Styles.dart";
 import "package:ass/Themes/ThemeColors.dart";
 import "package:ass/WIdgets/BlinkingDot.dart";
-import "package:http/http.dart";
+import "package:jwt_decoder/jwt_decoder.dart";
 import "package:provider/provider.dart";
 
 // ignore: must_be_immutable
@@ -78,10 +83,9 @@ class _StopWatchItemState extends State<StopWatchItem> {
                     },
                     child:  Icon(Icons.stop, color: ThemeColors.secondaryBackground,),
                 )
-                :const SizedBox(),
+                :ElevatedButton(style: Styles.secondaryButtonStyle, onPressed: () => addLabeledData(widget.selectedDevice, startTime, endTime), child: Icon(Icons.send, color: ThemeColors.secondaryBackground)),
                 IconButton(onPressed: () => _delete(), icon: Icon(Icons.delete, color: ThemeColors.textColor)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.send, color: ThemeColors.textColor)),
-
+          
               ],
             ),
           ],
@@ -133,15 +137,63 @@ class _StopWatchItemState extends State<StopWatchItem> {
 
       stopwatchItemsModel.removeStopwatchItem(widget);
       debugPrint("[---STOPWATCHES]" + stopwatchItemsModel.stopwatchItems.length.toString());
-            widget.onStopWatchItemDeleted();
+    widget.onStopWatchItemDeleted();
 
 
 
   }
-    
-
+  
   @override
   void dispose() {
     super.dispose();
+  }
+
+
+  Future<void> addLabeledData(DeviceDTO deviceDTO, DateTime start, DateTime end) async{
+      String? token = await APIController.getTokenFromSharedPreferences();
+
+
+    if(token != null){
+        Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+        if(JwtDecoder.isExpired(token)){
+          //add context
+        }else{
+
+           
+            //transforming the datetime into unix
+            var startTime = start.millisecondsSinceEpoch;
+            var endTime = end.microsecondsSinceEpoch;
+            // Unix-Timestamp als String
+            String startTimestamp = (startTime / 1000).toString();
+            String endTimestamp = (endTime / 1000).toString();
+
+            var startParts = startTimestamp.split('.');
+            String from = startParts[0];
+
+            var endParts = endTimestamp.split('.');
+            String to = endParts[0];
+
+           
+
+            String householdId = decodedToken['householdId'];
+            List<SmartMeterDataDTO> smartMeterData = await APIController.getSmartMeterData(token, householdId, from, to);
+
+            int response = await APIController.addLabeledData(token, householdId, deviceDTO!, smartMeterData);
+
+            if(response == StatusCodes.CREATED){
+              _delete();
+              BottomToast.showToast("New Label successfully added!");
+            }else if(response == StatusCodes.UNAUTHORIZED){
+              BottomToast.showToast("STH STRANGE HAPPENED: $response");
+            }else if(response == StatusCodes.INVALID_USER){
+              BottomToast.showToast("STH STRANGE HAPPENED: $response");
+            }else if(response == StatusCodes.SERVER_ERROR){
+              BottomToast.showToast("STH STRANGE HAPPENED: $response");
+            }else{
+              BottomToast.showToast("STH STRANGE HAPPENED: $response");
+            }
+        }
+    }
+
   }
 }
